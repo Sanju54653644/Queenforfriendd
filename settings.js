05212f3b6319f140e5667409956c9da7db33c815
Emptunknown <123576515+Emptunknown@users.noreply.github.com>
2023-03-25 16:54:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -96,7 +96,7 @@ global.NO_BLOCK = ["94715166712","94719574492"]
 //👇 Desable download words
 global.IMG_BLOCKER = ["xxx","sex","mia"]
 //👇 Open ai API
-global.CHATGPT_API = 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
+global.CHATGPT_API = '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'
 /*
 
    🍃 OTHER 🍃
